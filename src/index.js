c5ed1600e5d0a918d49a5801c7aca569c2bb23c4
Last Capricorn <48774366+LastCapricorn@users.noreply.github.com>
2023-05-30 02:37:07 +0200

    Complete startpage text
    - just needs some more CSS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import './style.css'
 import FavIcon from './img/lc_dark_red.svg'
 import Logo from './img/4d_burger_press_logo.png'
 import home from './startpage'
-let posX = 0
-let posY = 0
-let bgPY
+
 const head = document.querySelector('head')
 const body = document.querySelector('body')
 const iconLink = document.createElement('link')
@@ -14,12 +12,19 @@ const headerContainer = document.createElement('div')
 const restaurantLogo = document.createElement('img')
 const heading = document.createElement('h1')
 const nav = document.createElement('nav')
-const main = document.createElement('main')
-const footer = document.createElement('footer')
 const homeButton = document.createElement('button')
 const menuButton = document.createElement('button')
 const contactButton = document.createElement('button')
 const creditsButton = document.createElement('button')
+const main = document.createElement('main')
+const footer = document.createElement('footer')
+const footerContainer = document.createElement('div')
+const footerPara = document.createElement('p')
+const footerAnker = document.createElement('a')
+
+let posX = 0
+let posY = 0
+let bgPY
 
 iconLink.setAttribute('rel', 'shortcut icon')
 iconLink.setAttribute('href', FavIcon)
@@ -30,7 +35,6 @@ head.appendChild(gradient)
 restaurantLogo.setAttribute('src', Logo)
 restaurantLogo.setAttribute('alt', '4d Burger Press Logo')
 headerContainer.appendChild(restaurantLogo)
-
 heading.textContent = 'welcome at 4d burger press'
 headerContainer.appendChild(heading)
 header.appendChild(headerContainer)
@@ -40,13 +44,21 @@ homeButton.setAttribute('class', 'active')
 menuButton.textContent = 'menu'
 contactButton.textContent = 'contact'
 creditsButton.textContent = 'credits'
-
 nav.appendChild(homeButton)
 nav.appendChild(menuButton)
 nav.appendChild(contactButton)
 nav.appendChild(creditsButton)
 header.appendChild(nav)
+
 main.innerHTML =  home()
+
+footerPara.textContent = 'He@dcrafted 2023 by '
+footerAnker.textContent = 'Last Capricorn'
+footerAnker.setAttribute('href', 'https://github.com/LastCapricorn')
+footerPara.appendChild(footerAnker)
+footerContainer.appendChild(footerPara)
+footer.appendChild(footerContainer)
+
 body.appendChild(header)
 body.appendChild(main)
 body.appendChild(footer)
@@ -57,7 +69,7 @@ document.addEventListener('pointermove', (ev) => {
   bgPY = posY / document.documentElement.clientHeight * 100;
   gradient.innerHTML =`
   body {
-    background-image: radial-gradient(circle farthest-corner at ${posX}px ${posY}px, #000f2f 0%, #000018 33%, #000008 99%);
+    background-image: radial-gradient(circle farthest-corner at ${posX}px ${posY}px, var(--col10) 0%, var(--col11) 33%, var(--col12) 99%);
   }`
   header.style.backgroundPositionY = `${bgPY}%`
 })
--- src/startpage.js
+++ src/startpage.js
@@ -1,5 +1,25 @@
 export default function home() {
   return `
-  <p>This is the damn hottest burger joint in all of Night City and beyond!</p>
+  <div>
+    <h2>This is the damn hottest burger joint in all of Night City and beyond!</h2>
+    <h3>~ We are there for you 24/7 ~</h3>
+    <p>We only use the finest ingredients legally available in our burgers, so don't
+      believe any rumours:
+      We never process any meat stuff from Pacifica!</p>
+    <p>Our food printers work with BioTechnica synthetic flours only and we are the
+      first and so far the only ones to use the FaVIcon 3.2 (Fruit and Vegetable Ionic
+      Converter).</p>
+    <p>Due to our special house blend of seaweed and synth corn flour, our burger buns
+      are less green than others.</p>
+    <p>Of course, we also deliver our food directly to you, wherever you are - even in
+      the last corners of the badlands. For this we use retired and converted Militech
+      Griffin drones.</p>
+    <p>Each burger comes packaged in an autonomous microwave box.</p>
+    <p>Warning: Microwave boxes do not belong in the hands of children.</p>
+    <p>Warning: We are legally obliged to point out that autonomous microwave boxes may
+      not be used as weapons!</p>
+      <p>So take a look at our menu(excerpt) and join us in Watson.</p>
+      <p>WE PRINT YOUR PATTY!</p>
+  </div>
   `
 }
